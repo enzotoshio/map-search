564c052fe6269cf031ddf301f66799b0f9fa8f35
enzotoshio <enzo.toshiba@gmail.com>
2018-07-31 02:59:32 -0300

    removing service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { render } from "react-dom";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 import { configureStore } from "./redux/store";
 
 const store = configureStore();
