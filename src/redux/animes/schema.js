057a022bbcacef3a5f8273ce0eead9ea945edfd8
enzotoshio <enzo.toshiba@gmail.com>
2018-07-31 04:07:36 -0300

    fixing schema

--- src/redux/animes/schema.js
+++ src/redux/animes/schema.js
@@ -1,4 +1,4 @@
 import { schema } from "normalizr";
 
-export const anime = new schema.Entity("animes");
+export const anime = new schema.Entity("animes", {}, { idAttribute: "malId" });
 export const arrayOfAnimes = new schema.Array(anime);
